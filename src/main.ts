51e8c91a9e55378e54a45d4f2acea2b072486c54
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-04 19:44:38 -0300

    feat: use fetch

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,9 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { provideHttpClient, withFetch } from '@angular/common/http';
 
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
+provideHttpClient(withFetch());
+platformBrowserDynamic()
+  .bootstrapModule(AppModule)
+  .catch((err) => console.error(err));
