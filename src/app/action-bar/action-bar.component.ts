e4f61005f8782bb2070905bf52eeea2b04b54187
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-05 09:24:18 -0300

    feat: compose action-bar component

--- src/app/action-bar/action-bar.component.ts
+++ src/app/action-bar/action-bar.component.ts
@@ -7,6 +7,6 @@ import { Component, Input } from '@angular/core';
 })
 export class ActionBarComponent {
   @Input() title!: string;
-
+  @Input() actions!: string[];
   constructor() {}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,15 +11,33 @@ import { HttpClientModule } from '@angular/common/http';
 import { ActionBarComponent } from './action-bar/action-bar.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTableModule } from '@angular/material/table';
+import { ProductFiltersComponent } from './product-filters/product-filters.component';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
 
 @NgModule({
-  declarations: [AppComponent, TableProductComponent, ActionBarComponent],
+  declarations: [
+    AppComponent,
+    TableProductComponent,
+    ActionBarComponent,
+    ProductFiltersComponent,
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
     MatSlideToggleModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTableModule,
+    FormsModule,
+    MatFormFieldModule,
+    MatInputModule,
   ],
   providers: [provideClientHydration()],
   bootstrap: [AppComponent],
