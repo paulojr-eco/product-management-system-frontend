f45abab53acf5013a56b27898c4b9163c1cef4d8
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-05 09:25:59 -0300

    feat: compose product-filters component

---
+++ src/app/product-filters/product-filters.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product-filters',
+  templateUrl: './product-filters.component.html',
+  styleUrl: './product-filters.component.scss'
+})
+export class ProductFiltersComponent {
+
+}
