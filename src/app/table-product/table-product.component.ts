a68f12b3d4af9787df6125545ec040a00dd9d11a
Paulo Paiva <paulojr.eco@gmail.com>
2023-12-05 09:25:45 -0300

    feat: compose table-product component

--- src/app/table-product/table-product.component.ts
+++ src/app/table-product/table-product.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ProductService } from '../product/product.service';
+import { ProductService } from '../product-service/product.service';
 import { Product } from '../models/product.model';
 
 @Component({
@@ -9,12 +9,23 @@ import { Product } from '../models/product.model';
 })
 export class TableProductComponent implements OnInit {
   products: Product[] = [];
+  columnsToDisplay = ['codigo', 'descricao', 'custo'];
 
   constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.productService.getData().subscribe((products) => {
-      this.products = products.data;
+    this.productService.getData().subscribe((response) => {
+      if (response.body?.data) {
+        this.products = response.body.data;
+      }
+    });
+  }
+
+  onDelete(product: Product) {
+    this.productService.delete(product).subscribe((response) => {
+      if (response.status === 200) {
+        this.products = this.products.filter(p => p !== product);
+      }
     });
   }
 }
